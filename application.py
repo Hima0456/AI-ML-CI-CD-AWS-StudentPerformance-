44a03662e11d1c98d4fbd8412f5b0e70f3eda590
Hima0456 <143024559+Hima0456@users.noreply.github.com>
2025-09-02 19:01:29 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ from flask_cors import CORS,cross_origin
 import numpy as np
 import pandas as pd
 
-
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
